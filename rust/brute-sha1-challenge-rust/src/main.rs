463c7e263cc1a93902c3d078960686bfc42bb794
Wendell Magallanes <waveform.999@gmail.com>
2019-10-17 22:25:16 +1100

    fix rust code where it doesnt stop after match

--- rust/brute-sha1-challenge-rust/src/main.rs
+++ rust/brute-sha1-challenge-rust/src/main.rs
@@ -22,19 +22,35 @@ fn bruteforce(hash_string: &String, length: i32) {
     to_string_length_and_hash(&"abcdefghijklmnopqrstuvwxyz".chars().collect(), "".to_string(), length, &hash_string);
 }
 
-fn to_string_length_and_hash(chars: &Vec<char>, prefix: String, length: i32, hash_string: &String) {
+fn to_string_length_and_hash(chars: &Vec<char>, prefix: String, length: i32, hash_string: &String) -> bool {
     if length == 0 {
         let mut hasher = Sha1::new();
         hasher.input_str(&prefix);
+
         let hex = &*hasher.result_str();
         if hex == hash_string {
             println!("Success! string = {}, hash = {}", prefix, hex);
+            return true
         }
-        return
+        return false
     }
 
     for i in 0..chars.len() {
         let new_prefix = format!("{}{}", prefix, chars[i]);
-        to_string_length_and_hash(chars, new_prefix, length-1, hash_string);
+
+        // slowest concatenation
+        // let mut new_prefix = String::from(&*prefix);
+        // new_prefix.push(chars[i]);
+
+        // 2nd fastest concatenation
+        // let mut new_prefix = prefix.to_owned();
+        // new_prefix.push(chars[i]);
+
+        let found = to_string_length_and_hash(chars, new_prefix, length-1, hash_string);
+
+        if found {
+            return true
+        }
     }
+    return false
 }
